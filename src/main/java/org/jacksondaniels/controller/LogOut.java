5dd09c8c01d9e1eb03e925c57b0c13d797e9622f
Jackson Daniels <jaydigitydawg@gmail.com>
2022-05-06 14:37:36 -0500

    Fixing login and logout

--- src/main/java/org/jacksondaniels/controller/LogOut.java
+++ src/main/java/org/jacksondaniels/controller/LogOut.java
@@ -1,29 +1,32 @@
 package org.jacksondaniels.controller;
 
+import org.jacksondaniels.util.PropertiesLoader;\
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jacksondaniels.util.PropertiesLoader;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Properties;
 
 
 
-/** Begins the authentication process using AWS Cognito
- *
+/**
+ * Begins the authentication process using AWS Cognito
  */
-@WebServlet(urlPatterns = {"/logIn"})
-public class LogIn extends HttpServlet implements PropertiesLoader {
+@WebServlet(urlPatterns = {"/logOut"})
+public class LogOut extends HttpServlet implements PropertiesLoader {
     Properties properties;
     private final Logger logger = LogManager.getLogger(this.getClass());
     public static String CLIENT_ID;
     public static String LOGIN_URL;
     public static String REDIRECT_URL;
+    public static String LOGOUT_URL;
+    public static String LOGOUT_URI;
 
     @Override
     public void init() throws ServletException {
@@ -35,14 +38,14 @@ public class LogIn extends HttpServlet implements PropertiesLoader {
      * Read in the cognito props file and get the client id and required urls
      * for authenticating a user.
      */
-    // TODO This code appears in a couple classes, consider using a startup servlet similar to adv java project
-    // 4 to do this work a single time and put the properties in the application scope
     private void loadProperties() {
         try {
             properties = loadProperties("/cognito.properties");
             CLIENT_ID = properties.getProperty("client.id");
             LOGIN_URL = properties.getProperty("loginURL");
             REDIRECT_URL = properties.getProperty("redirectURL");
+            LOGOUT_URL = properties.getProperty("logoutURL");
+            LOGOUT_URI = properties.getProperty("signOutURL");
         } catch (IOException ioException) {
             logger.error("Cannot load properties..." + ioException.getMessage(), ioException);
         } catch (Exception e) {
@@ -60,7 +63,15 @@ public class LogIn extends HttpServlet implements PropertiesLoader {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO if properties weren't loaded properly, route to an error page
-        String url = LOGIN_URL + "?response_type=code&client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URL;
-        resp.sendRedirect(url);
+
+        HttpSession session = req.getSession();
+        try {
+            String url = LOGOUT_URL + "?client_id=" + CLIENT_ID + "&logout_uri=" + LOGOUT_URI;
+            session.invalidate();
+            resp.sendRedirect(url);
+        } catch (Exception e) {
+            logger.error("Error loading properties" + e.getMessage(), e);
+            resp.sendRedirect("/generror");
+        }
     }
 }
--- src/main/webapp/header.jsp
+++ src/main/webapp/header.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary rounded">
@@ -11,7 +12,23 @@
                 <div class="navbar-nav">
                     <a class="nav-link active" aria-current="page" href="index.jsp">Home</a>
                     <a class="nav-link" href="<%=request.getContextPath()%>/viewGames">View Reviews</a>
-                    <a class="nav-link" href="addGame.jsp">Add Review</a>
+                    <c:choose>
+                        <c:when test="${username == null}">
+                            <a href="nav-link" href="/logIn">Login</a>
+                        </c:when>
+                        <c:otherwise>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/addReview.jsp"><c:out value=" ${userName}"/> </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/viewProfile"><c:out value=" ${userName}"/> </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/logOut">Log Out</a>
+                            </li>
+                        </c:otherwise>
+
+                    </c:choose>
 
                 </div>
             </div>
