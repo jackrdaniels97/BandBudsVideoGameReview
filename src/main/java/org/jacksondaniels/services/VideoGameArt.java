957a2605189d98ae3a1c54de3e7e1f4045142c2f
Jackson Daniels <jaydigitydawg@gmail.com>
2022-05-08 19:06:00 -0500

    Creating class to look at api to get video game art

---
+++ src/main/java/org/jacksondaniels/services/VideoGameArt.java
@@ -0,0 +1,9 @@
+package org.jacksondaniels.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VideoGameArt {
+}
