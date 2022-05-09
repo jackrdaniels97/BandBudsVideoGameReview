bcd599f74b408b4a1cf51a5cf16a21e3e5faffd1
Jackson Daniels <jaydigitydawg@gmail.com>
2022-05-09 11:04:30 -0500

    Fixing spelling

--- src/main/java/org/jacksondaniels/controller/LogOut.java
+++ src/main/java/org/jacksondaniels/controller/LogOut.java
@@ -9,69 +9,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Properties;
-
-
 
 /**
- * Begins the authentication process using AWS Cognito
+ *  Contains method for logging out user
  */
 @WebServlet(urlPatterns = {"/logOut"})
-public class LogOut extends HttpServlet implements PropertiesLoader {
-    Properties properties;
+/* Begins the authentication process using AWS Cognito */
+public class LogOut extends HttpServlet {
     private final Logger logger = LogManager.getLogger(this.getClass());
-    public static String CLIENT_ID;
-    public static String LOGIN_URL;
-    public static String REDIRECT_URL;
-    public static String LOGOUT_URL;
-    public static String LOGOUT_URI;
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        loadProperties();
-    }
 
     /**
-     * Read in the cognito props file and get the client id and required urls
-     * for authenticating a user.
-     */
-    private void loadProperties() {
-        try {
-            properties = loadProperties("/cognito.properties");
-            CLIENT_ID = properties.getProperty("client.id");
-            LOGIN_URL = properties.getProperty("loginURL");
-            REDIRECT_URL = properties.getProperty("redirectURL");
-            LOGOUT_URL = properties.getProperty("logoutURL");
-            LOGOUT_URI = properties.getProperty("signOutURL");
-        } catch (IOException ioException) {
-            logger.error("Cannot load properties..." + ioException.getMessage(), ioException);
-        } catch (Exception e) {
-            logger.error("Error loading properties" + e.getMessage(), e);
-        }
-    }
-
-    /**
-     * Route to the aws-hosted cognito login page.
-     * @param req servlet request
-     * @param resp servlet response
-     * @throws ServletException
-     * @throws IOException
+     * Called by server to allow servlet to handle a GET request
+     *
+     * @param req               object containing req client has made of the servlet
+     * @param resp              object that containing resp servlet sends to the client
+     * @throws ServletException if an input or output error is detected when handling GET req
+     * @throws IOException      if the request for the GET could not be handled
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO if properties weren't loaded properly, route to an error page
-
-        HttpSession session = req.getSession();
-        try {
-            String url = LOGOUT_URL + "?client_id=" + CLIENT_ID + "&logout_uri=" + LOGOUT_URI;
-            session.invalidate();
-            resp.sendRedirect(url);
-        } catch (Exception e) {
-            logger.error("Error loading properties" + e.getMessage(), e);
-            resp.sendRedirect("/generror");
-        }
+        req.getSession().invalidate(); // end session to log user out
+        // send user to home
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
